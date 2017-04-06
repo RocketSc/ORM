db856fd85deee35bbae238cf7d27d9092b6ce057
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-06 17:24:11 +0300

    Creating and implementing Database class

--- app/Database.php
+++ app/Database.php
@@ -2,7 +2,26 @@
 
 namespace App;
 
+use PDO;
+
 class Database
 {
+    private static $instance;
 
+    public static function getConnection()
+    {
+        if ( !empty(self::$instance) ) {
+            return self::$instance;
+        }
+
+        $parameters = include ROOT . '/config/database.php';
+
+        $dsn = 'mysql:host=' . $parameters['host'] . ';dbname=' . $parameters['dbname'];
+        $db = new PDO($dsn, $parameters['user'], $parameters['password']);
+        $db->exec('set names utf8');
+
+        self::$instance = $db;
+
+        return self::$instance;
+    }
 }
